aae471d201a81e09d60c926f9e183a7c3a679aaf
Jaxongir <jmuxutdinov@mail.ru>
2021-10-29 04:41:54 +0500

    traffic light

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export const App = () => {
+  const [temperature, setTemperature] = useState(0);
+  const [temperatureColor, setTemperatureColor] = useState("red");
+  const Plus = () => {
+    const newTamperature = temperature + 1;
+    if (newTamperature > 10) return;
+    setTemperature(newTamperature);
+    if (newTamperature <= 3) {
+      setTemperatureColor("red");
+    } else if (newTamperature <= 6) {
+      setTemperatureColor("yellow");
+    } else {
+      setTemperatureColor("green");
+    }
+  };
+  const Minus = () => {
+    const newTamperature = temperature - 1;
+    if (newTamperature < 0) return;
+    setTemperature(newTamperature);
+    if (newTamperature <= 3) {
+      setTemperatureColor("red");
+    } else if (newTamperature <= 6) {
+      setTemperatureColor("yellow");
+    } else {
+      setTemperatureColor("green");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <div className="Wrapper">
+        <div className={`Wrap ${temperatureColor}`}>
+          <h1>{temperature}</h1>
+        </div>
+        <div className="btn-wrapper">
+          <button className="button" onClick={() => Minus()}>
+            -
+          </button>
+          <button className="button" onClick={() => Plus()}>
+            +
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
